2171c1e25b9739ea73390d798bc9e68d3318ae51
Britt Carter <brittbmh@gmail.com>
2019-06-04 15:05:54 -0500

    added two components and bootstrap

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled><b>Fancy Quiz</b></b-nav-item>
+      <b-nav-item disabled>Counter: 2/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron header="BootstrapVue" lead="Bootstrap 4 Components for Vue.js 2">
+      <template slot="lead">
+        Some question??
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        All the answers
+      </p>
+
+      <b-button variant="primary" href="#">Do Something</b-button>
+      <b-button variant="success" href="#">Do Something Else</b-button>
+    </b-jumbotron>
+  </div>
+</template>
