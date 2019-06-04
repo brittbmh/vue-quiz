14130c4fede71d820b13f8cd4fe59724c8254ad9
Britt Carter <brittbmh@gmail.com>
2019-06-04 15:51:31 -0500

    display questions from API

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="question-box-container">
-    <b-jumbotron header="BootstrapVue" lead="Bootstrap 4 Components for Vue.js 2">
+    <b-jumbotron>
       <template slot="lead">
-        Some question??
+        {{ currentQuestion.question }}
       </template>
 
       <hr class="my-4">
@@ -11,8 +11,22 @@
         All the answers
       </p>
 
-      <b-button variant="primary" href="#">Do Something</b-button>
-      <b-button variant="success" href="#">Do Something Else</b-button>
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next Q</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    },
+    mounted() {
+        console.log(this.currentQuestion);
+        
+    }
+}
+</script>
+
