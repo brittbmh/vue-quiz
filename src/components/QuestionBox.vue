3cd9a1277d4fe36626f0394639cc35e80b139759
Britt Carter <brittbmh@gmail.com>
2019-06-05 14:34:30 -0500

    added selection color to buttons

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-      <template slot="lead">
-        {{ currentQuestion.question }}
-      </template>
+      <template slot="lead">{{ currentQuestion.question }}</template>
 
       <hr class="my-4">
-
-      <p>
-        All the answers
-      </p>
-
+      <b-list-group>
+        <b-list-group-item 
+            v-for="(answer, index) in answers" 
+            :key="index" 
+            @click="selectAnswer(index)"
+            :class="[selectedIndex === index ? 'selected' : '']"
+            >{{answer}}
+            </b-list-group-item>
+      </b-list-group>
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next Q</b-button>
     </b-jumbotron>
@@ -19,14 +21,58 @@
 
 <script>
 export default {
-    props: {
-        currentQuestion: Object,
-        next: Function
-    },
-    mounted() {
-        console.log(this.currentQuestion);
-        
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+      return {
+          selectedIndex: null
+      }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
     }
-}
+  },
+  methods: {
+      selectAnswer(index){
+          this.selectedIndex = index
+      }
+  },
+  mounted() {
+    console.log(this.currentQuestion);
+  }
+};
 </script>
+
+<style scoped>
+.list-group {
+    margin-bottom: 15px;    
+}
+
+.list-group-item:hover {
+    background: #EEE;
+    cursor: pointer;
+}
+
+.btn {
+    margin: 0 5px;
+}
+
+.selected {
+    background-color: dodgerblue;
+}
+
+.correct {
+    background-color: lightgreen;
+}
+
+.incorrect {
+    background-color: tomato;
+}
+</style>
+
 
